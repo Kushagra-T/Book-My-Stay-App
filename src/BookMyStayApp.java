40466a8a920cf53e72a1f2dc9088f412509d7fce
Kushagra-T <kushagrasty@gmail.com>
2026-04-09 00:19:02 +0530

    UC1: Add welcome message module

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,8 @@
+public class BookMyStayApp {
+
+    private static final String version = "v1.0";
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Hotel Booking System " + version);
+        System.out.println("Application initialized successfully.");
+    }
+}
